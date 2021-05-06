b96121834a0d1391c71225d0cfc7e7f12e10d4d6
Zachary Hoffman <hoffking@gmail.com>
2021-05-06 10:28:06 -0400

    Update index.js

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,14 +1,7 @@
 import firebase from "firebase"
 
 const config = {
-    apiKey: "AIzaSyBYHQLxIlNUnqJQxAzjzFjpTai1kyDot0I",
-    authDomain: "circles-20e13.firebaseapp.com",
-    databaseURL: "https://circles-20e13.firebaseio.com",
-    projectId: "circles-20e13",
-    storageBucket: "circles-20e13.appspot.com",
-    messagingSenderId: "634972235208",
-    appId: "1:634972235208:web:12f429dc28f82403dc8cfd",
-    measurementId: "G-PEFJHWRMXT"
+
   };
 firebase.initializeApp(config);
 const auth = firebase.auth;
